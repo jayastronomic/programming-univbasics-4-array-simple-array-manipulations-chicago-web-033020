c92591883e8de0659cf37c0bba26addec2dd27a5
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-10 02:43:55 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+  array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def using_pop_args(array)
+  array.pop(2)
+end
+
+def using_shift
